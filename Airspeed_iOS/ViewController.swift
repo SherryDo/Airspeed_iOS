3b0e12ad58d41a9d5507e990563d85b09afaba3f
dowhil.net <dowhile_net@sina.com>
2015-06-23 10:09:46 +0800

    初始化
    初始化

---
+++ Airspeed_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airspeed_iOS
+//
+//  Created by xiaolei on 15/6/23.
+//  Copyright © 2015年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
