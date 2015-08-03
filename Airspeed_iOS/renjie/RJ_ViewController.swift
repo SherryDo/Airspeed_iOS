1e9d450707be1293ae4dcd0e16a3c646bfdced62
chouxun <chou.xun@qq.com>
2015-08-03 19:38:14 +0800

    RJ_ViewController
    什么是本地储存

---
+++ Airspeed_iOS/renjie/RJ_ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  Airspeed_iOS
+//
+//  Created by 仇寻 on 15/8/3.
+//  Copyright © 2015年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+
+class RJ_ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
