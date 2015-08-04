cfc3f77fdf5c51370f4b19a20f167111bfe39875
chouxun <chou.xun@qq.com>
2015-08-04 14:43:06 +0800

    我再提交一下试试。。。
    再打不开就不是我的错了

---
+++ Airspeed_iOS/renjie/RJ_SJViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RJ_SJViewController.swift
+//  Airspeed_iOS
+//
+//  Created by 仇寻 on 15/8/4.
+//  Copyright © 2015年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+class RJ_SJViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+}
