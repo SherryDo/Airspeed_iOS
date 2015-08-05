ba5c56e79ed4e41c4950d4445bcbd6f731589714
Cold Graphics <root@dowhile.net>
2015-08-05 22:35:37 +0800

    error

---
+++ Airspeed_iOS/wangzhuo/WZ_onSaleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WZ_onSaleViewController.swift
+//  Airspeed_iOS
+//
+//  Created by 王卓 on 15/8/5.
+//  Copyright © 2015年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+class WZ_onSaleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title="我的上架"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
