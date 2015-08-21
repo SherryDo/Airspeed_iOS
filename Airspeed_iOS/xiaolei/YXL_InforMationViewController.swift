f06967b42bf3b2768fe00e4715726e0734580485
Cold Graphics <root@dowhile.net>
2015-08-21 12:20:46 +0800

    🎺🎺🎺

--- Airspeed_iOS/xiaolei/YXL_InforMationViewController.swift
+++ Airspeed_iOS/xiaolei/YXL_InforMationViewController.swift
@@ -24,14 +24,5 @@ class YXL_InforMationViewController: UIViewController,ENSideMenuDelegate{
    func sideMenuShouldOpenSideMenu () -> Bool{
     return false
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
