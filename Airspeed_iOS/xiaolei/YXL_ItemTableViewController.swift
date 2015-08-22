444e590632172ab92c626c6dc13e1e7ae9291f40
Cold Graphics <root@dowhile.net>
2015-08-22 22:54:03 +0800

    😄

--- Airspeed_iOS/xiaolei/YXL_ItemTableViewController.swift
+++ Airspeed_iOS/xiaolei/YXL_ItemTableViewController.swift
@@ -13,11 +13,7 @@ class YXL_ItemTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    
     }
 
     override func didReceiveMemoryWarning() {
