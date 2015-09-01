ef5ce9d00cfacec2b6c1f0e94691a95bc18b5a20
WangZhuo2015 <280034816@qq.com>
2015-09-01 09:21:41 +0800

    小修改
    小修改

---
+++ Airspeed_iOS/wangzhuo/WZ_itemDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WZ_itemDetailViewController.swift
+//  Airspeed_iOS
+//
+//  Created by 王卓 on 15/8/27.
+//  Copyright © 2015年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+class WZ_itemDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
